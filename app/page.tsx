e9fd6442a19b02320d785c4e531218e34766493a
bognevivien <bognetienoue@gmail.com>
2024-02-08 13:16:39 +0100

    init shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <h1 className="">lets build disney</h1>
+      <Button variant={"link"}>Click Me</Button>
     </main>
   );
 }
